d5a42fb11c8f2877206bc0903e9f4e25ddffb73f
Joshua <Joshua@192.168.1.25>
2017-12-11 17:36:54 -0500

    Initial Push to Master

---
+++ LabRat-desktop/src/com/labrat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.labrat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.labrat.LabRat;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LabRat(), config);
+	}
+}
