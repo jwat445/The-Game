ecd471dc704f976c511fb3a5a73f23bb98750d5c
Joshua <Joshua@192.168.1.25>
2017-12-18 00:43:42 -0500

    Added comprehensive test coverage

---
+++ LabRat-core/test/backend_tests/CoreTest.java
@@ -0,0 +1,15 @@
+package javatests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoreTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+		System.out.println("Everything works great. Nice job.");
+	}
+
+}
